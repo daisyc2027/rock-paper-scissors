02c80116417a70f0deec1e5702a5828fb99648c4
daisychen <daisyc2027@student.cis.edu.hk>
2024-09-11 07:42:15 +0800

    first version of the rock paper scissors game

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,81 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        //Here we will calculate the winner of the game. 0 means that its a tie, 1 is user wins and 2 is opponents win.
-
-        int userScore = 0;
-        int opponentScore = 0;
-
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> userChoices = new ArrayList<>();
+        ArrayList<String> computerChoices = new ArrayList<>();
+
         System.out.println("How many rounds would you like to play?");
-        int howManyRounds = sc.nextInt();
+        int numberRounds = sc.nextInt();
+        sc.nextLine();
 
-        for (int firstToThree = 1; firstToThree <= howManyRounds; firstToThree++) {
-            int winOrLose = playGame();
-            if (winOrLose == 0) {
-                System.out.println("round " + firstToThree + ": its a tie!");
-            } else if (winOrLose == 1) {
-                ++userScore;
-                System.out.println("round " + firstToThree + ": You win this round!");
-            } else if (winOrLose == 2) {
-                ++opponentScore;
-                System.out.println("round " + firstToThree + ": You win this round!");
+        for (int i = 0; i < numberRounds; i++) {
+            int userScore = 0;
+            int computerScore = 0;
+            //rock will be 0, paper 1, scissors 2
+            String[] options = {"rock", "paper", "scissors"};
+            int userChoiceIndex;
+            String userChoice;
+            String computerChoice;
+
+            System.out.println("lets start! rock, paper, scissors shoot!");
+            userChoice = sc.nextLine();
+            while (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+                System.out.println("Please enter a valid argument");
+                userChoice = sc.nextLine();
             }
+            userChoiceIndex = convertingUserChoice(userChoice);
+            userChoices.add(options[userChoiceIndex]);
+
+            int computerChoiceIndex = (int) (Math.random() * 2);
+            computerChoice = convertingComputerChoice(computerChoiceIndex);
+            computerChoices.add(options[computerChoiceIndex]);
+
+            int a = Integer.parseInt(userChoices.get(i));
+            int b = Integer.parseInt(computerChoices.get(i));
+
+
+            if (a == b) {
+                System.out.println("its a tie! the computer chose: " + computerChoice);
+            }
+            else if(a == 0 && b == 2 || a == 1 && b == 0 || a == 2 && b ==1){
+                ++userScore;
+                System.out.println("You win this round! The score is: " + userScore + ", " + computerScore +
+                        "\n The computer chose: " + computerChoice);
+            }
+            else{
+                ++computerScore;
+                System.out.println("You lose this round! The score is: " + userScore + ", " + computerScore +
+                        "\n The computer chose: " + computerChoice);
+            }
+
         }
-        System.out.println("The final scores are: " + userScore + " to you, " + opponentScore + " to your opponent.");
+
     }
 
-    public static int playGame(){
-        Scanner sc = new Scanner(System.in);
 
-        //rock will be 0, paper 1, scissors 2
-
-        String rockPaperScissors;
-        int userPlay;
-        int opponentsPlay = (int) (Math.random()*3);
-        System.out.println("rock, paper, or scissors?");
-        rockPaperScissors = sc.nextLine();
-        while(!rockPaperScissors.equals("rock")&&!rockPaperScissors.equals("paper")&&!rockPaperScissors.equals("scissors")) {
-            System.out.println("please input a valid argument");
-            rockPaperScissors = sc.nextLine();
-        }
-        if (rockPaperScissors.equals("rock")){
-            userPlay = 0;
-        }
-        else if(rockPaperScissors.equals("paper")){
-            userPlay = 1;
-        }
-        else{
-            userPlay = 2;
+    public static String convertingComputerChoice(int computerChoiceIndex){
+        if (computerChoiceIndex == 0) {
+            return "rock";
+        } else if (computerChoiceIndex == 1) {
+            return "paper";
+        } else {
+            return "scissors";
         }
 
-        return gameResults(userPlay, opponentsPlay);
     }
 
-
-    //Here we will calculate the winner of the game. 0 means that it's a tie, 1 is user wins and 2 is opponents win.
-    public static int gameResults(int userPlay, int opponentsPlay){
-        if(userPlay == opponentsPlay){
+    public static int convertingUserChoice(String userChoice){
+        if (userChoice.equals("rock")) {
             return 0;
-        }
-        else if(userPlay - opponentsPlay == 1 || userPlay - opponentsPlay == -2){
+        } else if (userChoice.equals("paper")) {
             return 1;
-        }
-        else{
+        } else {
             return 2;
         }
     }
+
 }
+
